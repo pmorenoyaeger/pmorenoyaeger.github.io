fa7059067f851f1f1991ae43550f2c7cb79cf154
pmorenoyaeger <130818695+pmorenoyaeger@users.noreply.github.com>
2026-05-30 22:23:48 +0000

    Deploying to gh-pages from @ pmorenoyaeger/pmorenoyaeger.github.io@8ee4607fd387400060e02bbdada3cf3d5517df94 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,11 +445,13 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-announcement-3",
+          title: 'Announcement_3',
           description: "",
-          section: "News",},{id: "news-i-succesfully-defended-my-phd",
-          title: 'I succesfully defended my PhD!',
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_3/";
+            },},{id: "news-i-succesfully-defended-my-phd",
+          title: 'I succesfully defended my PhD',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
