fe4a45d7599e9a8b5e88a757870828783f470a62
pmorenoyaeger <130818695+pmorenoyaeger@users.noreply.github.com>
2026-05-30 23:11:39 +0000

    Deploying to gh-pages from @ pmorenoyaeger/pmorenoyaeger.github.io@0a2a62b33c1af6f5ec530bdc1456c2d31c970dae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -485,7 +485,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%6F%72%65%6E%6F%79%61%65%67%65%72@%77%69%73%63.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -506,14 +506,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=uoa0ozMAAAAJ&hl", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://pmorenoyaeger.github.io/", "_blank");
         },
       },{
       id: 'light-theme',
