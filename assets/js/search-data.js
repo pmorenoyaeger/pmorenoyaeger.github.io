29f9484b793dda6e39d925a849493916b15578fe
pmorenoyaeger <130818695+pmorenoyaeger@users.noreply.github.com>
2026-05-31 16:47:03 +0000

    Deploying to gh-pages from @ pmorenoyaeger/pmorenoyaeger.github.io@fdfd423e473e026df52cd24a080ee183cf0be7de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-publications",
+          title: "Publications",
+          description: "Publications by year. More details in my Google Scholar.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
